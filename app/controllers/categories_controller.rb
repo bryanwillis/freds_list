c2ab49bd19089baa1efe037212f5eef8adbd3499
John Figueiredo <john@johnfigueiredo.com>
2013-06-27 10:28:54 -0700

    Added home page and made root path

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,15 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def home
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @categories }
+    end
+  end
+
   # GET /categories/1
   # GET /categories/1.json
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ FredsList2::Application.routes.draw do
   resources :posts
   resources :categories
 
-  root to: 'categories#index'
+  root to: 'categories#home'
 
   match '/categories',  to: 'categories#index'
   match '/posts',       to: 'posts#index'
